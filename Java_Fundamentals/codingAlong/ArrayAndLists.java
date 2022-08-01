469b3098d1f7d2dab4f0689adcade4a2ad6b4ee4
Adrian Dowst <aadowst@gmail.com>
2022-08-01 15:38:54 -0700

    initial commit

---
+++ Java_Fundamentals/codingAlong/ArrayAndLists.java
@@ -0,0 +1,9 @@
+class ArrayAndLists{
+    public static void main(String[] args){
+        String[] fruits = new String[5];
+
+        for (int i =0; i<fruits.length; i++){
+            System.out.println(fruits[i]);
+        }
+    }
+}
