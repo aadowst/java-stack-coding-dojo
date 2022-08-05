5d8621e19823b07c7bb6e53da3d35d4039f37997
Adrian Dowst <aadowst@gmail.com>
2022-08-05 09:22:41 -0700

    initial commit

---
+++ SpringProjects/helloWorld/src/main/java/com/aadowst/helloworld/HomeController.java
@@ -0,0 +1,34 @@
+package com.aadowst.helloworld;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+
+
+@RequestMapping("/")
+public String index(@RequestParam(value="q", required=false)String searchQuery) {
+	if(searchQuery == null) {
+		return "you searched for nothing";
+	
+	}else {
+		
+		return "You searched for:  " + searchQuery;
+	}
+}
+
+@RequestMapping("m/{track}/{module}/{lesson}")
+public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson) {
+	return "Track:  " + track  + ",Module: "  + module  +  ", Lesson:  "  + lesson;
+}
+
+@RequestMapping("/world")
+public String world() {
+	return "Class level annotations are cool, too!";
+}
+
+}
